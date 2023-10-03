9fba7191d886312db4b1ccef84f4a54ff21116be
Bhoomika <bhoomikahm18@gmail.com>
2023-10-03 12:14:06 +0530

    Creating connection with mysql

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const mysql = require('mysql');
 const express = require('express');
-
+const studentRouter = require('./routers/student-router.js');
+const connection = require('./models/Connection.js');
 
 let app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "college"
-
-});
 
 connection.connect(err => {
     if (err) console.log("There is an error while connecting" + err);
@@ -23,19 +17,4 @@ connection.connect(err => {
 
 })
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/register.html');
-});
-
-app.post('/submit', (req, res) => {
-    console.log(req.body);
-    const { name, email, mobile } = req.body;
-
-    let sql_query = `INSERT INTO STUDENTS (NAME, EMAIL, MOBILE) VALUES('${name}', '${email}', '${mobile}');`;
-    connection.query(sql_query, (err, result) => {
-        if (err) return console.log("Error while inserted data " + err);
-        res.json({ 'message ': "insert was successfull", "result": result.insertId });
-    });
-
-});
+app.use('/', studentRouter);
---
+++ routers/student-router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const { homePage, insertValues } = require("../controllers/student-contoller.js");
+
+const studentRouter = express.Router();
+
+studentRouter.get("/", homePage);
+studentRouter.post("/submit", insertValues);
+
+module.exports = studentRouter;
